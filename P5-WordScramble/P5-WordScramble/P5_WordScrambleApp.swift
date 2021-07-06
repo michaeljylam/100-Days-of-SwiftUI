624bbfeaf58880aa02037d15be9a39cfdbb08c86
michaeljylam <michael.lam8@live.com>
2021-07-05 22:44:53 -0400

    Create WordScamble

---
+++ P5-WordScramble/P5-WordScramble/P5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  P5_WordScrambleApp.swift
+//  P5-WordScramble
+//
+//  Created by Michael Lam on 2021-07-05.
+//
+
+import SwiftUI
+
+@main
+struct P5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
